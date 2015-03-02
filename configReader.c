fbed856eeef1480ed2181c269691d19fa3e453d5
Ben Scholer <benscholer3248511@gmail.com>
2015-03-02 10:23:07 -0500

    Andrew added stuff

--- configReader.c
+++ configReader.c
@@ -2,21 +2,38 @@
 #include <string.h>
 #include <stdlib.h>
 
-struct entry {
-  
-} // end of entry struct
+enum DataType {
+	type_int,
+	type_float,
+	type_string
+}; // end of dataTypes enum
 
-  enum dataTypes {
-    type_int,
-    type_float,
-    type_string
-  } // end of dataTypes enum
+union Val {
+	int integer;
+	float floater;
+	char* stringer;
+}; // end of val union
 
-    union val {
-      int data_type;
-      char name[20];
-    } // end of val union
+struct Entry {
+	enum DataType dataType;
+	union Val value;
+}; // end of entry struct
 
 int main(void) {
+	struct Entry entry;
 
+	entry.dataType = type_int;
+	entry.value.integer = 4;
+	
+	switch (entry.dataType) {
+		case type_int:
+			printf("%d\n", entry.value.integer);
+			break;
+
+		case type_float:
+			printf("%f\n", entry.value.floater);
+
+		case type_string:
+			printf("%s\n", entry.value.stringer);
+	}
 } // end of main function
